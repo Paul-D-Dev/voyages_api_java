c7185a4e6c3154d262ee1c71ec4a93a52519c845
Paul DAO <dao.van.paul@gmail.com>
2024-02-23 12:55:48 -0500

    first commit

---
+++ src/main/java/com/example/voyages_api/VoyagesApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.voyages_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VoyagesApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VoyagesApiApplication.class, args);
+	}
+
+}
