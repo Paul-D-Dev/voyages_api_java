3fba2cf3e82d28a4d701fec2dff3df61ace23487
Paul DAO <dao.van.paul@gmail.com>
2024-02-26 13:55:13 -0500

    TESt user find one

--- src/test/java/com/example/voyages_api/controllers/UserControllerIntegrationTests.java
+++ src/test/java/com/example/voyages_api/controllers/UserControllerIntegrationTests.java
@@ -2,6 +2,7 @@ package com.example.voyages_api.controllers;
 
 import com.example.voyages_api.TestDataUtil;
 import com.example.voyages_api.domain.entities.UserEntity;
+import com.example.voyages_api.services.UserService;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -21,12 +22,15 @@ import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 @AutoConfigureMockMvc
 public class UserControllerIntegrationTests {
 
+    private final UserService service;
+
     private final MockMvc mvc;
 
     private final ObjectMapper objectMapper;
 
     @Autowired
-    public UserControllerIntegrationTests(MockMvc mvc) {
+    public UserControllerIntegrationTests(MockMvc mvc, UserService service) {
+        this.service = service;
         this.mvc = mvc;
         objectMapper = new ObjectMapper();
     }
@@ -60,6 +64,44 @@ public class UserControllerIntegrationTests {
                 MockMvcResultMatchers.jsonPath("$.id").isNumber()
         ).andExpect(
                 MockMvcResultMatchers.jsonPath("$.name").value("Georges")
+        );
+    }
+
+    @Test
+    public void TestThatFoundUserSuccessfullyReturnsHttpStatus200WhenUserExists() throws Exception {
+        UserEntity user = TestDataUtil.createTestUserEntityA();
+        service.create(user);
+
+        mvc.perform(
+                MockMvcRequestBuilders.get("/users/1")
+                        .contentType(MediaType.APPLICATION_JSON)
+        ).andExpect(
+                MockMvcResultMatchers.status().isOk()
+        );
+    }
+
+    @Test
+    public void testThatGetAuthorReturnsAuthorWhenAuthorExists() throws Exception {
+        UserEntity user = TestDataUtil.createTestUserEntityA();
+        service.create(user);
+
+        mvc.perform(
+                MockMvcRequestBuilders.get("/users/1")
+                        .contentType(MediaType.APPLICATION_JSON)
+        ).andExpect(
+                MockMvcResultMatchers.jsonPath("$.id").value(1)
+        ).andExpect(
+                MockMvcResultMatchers.jsonPath("$.name").value("Georges")
+        );
+    }
+
+    @Test
+    public void TestThatFoundUserFailedAndReturnsHttpStatus404WhenNoUserExists() throws Exception {
+        mvc.perform(
+                MockMvcRequestBuilders.get("/users/403")
+                        .contentType(MediaType.APPLICATION_JSON)
+        ).andExpect(
+                MockMvcResultMatchers.status().isNotFound()
         );
     }
 }
