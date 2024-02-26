c0614230603b3a0437c8737f77e01a98714acd50
Paul DAO <dao.van.paul@gmail.com>
2024-02-26 11:17:06 -0500

    FEAT implements UserMapper

---
+++ src/main/java/com/example/voyages_api/mappers/impl/UserMapper.java
@@ -0,0 +1,26 @@
+package com.example.voyages_api.mappers.impl;
+
+import com.example.voyages_api.domain.dto.UserDto;
+import com.example.voyages_api.domain.entities.UserEntity;
+import com.example.voyages_api.mappers.Mapper;
+import org.modelmapper.ModelMapper;
+
+public class UserMapper implements Mapper<UserEntity, UserDto> {
+
+    private final ModelMapper modelMapper;
+
+    public UserMapper(ModelMapper modelMapper) {
+        this.modelMapper = modelMapper;
+    }
+
+
+    @Override
+    public UserDto mapTo(UserEntity userEntity) {
+        return modelMapper.map(userEntity, UserDto.class);
+    }
+
+    @Override
+    public UserEntity mapFrom(UserDto userDto) {
+        return modelMapper.map(userDto, UserEntity.class);
+    }
+}
