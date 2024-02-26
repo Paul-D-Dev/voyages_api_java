abd37e3d81fbbd0d96c21f9d6e9edbdee3a019a0
Paul DAO <dao.van.paul@gmail.com>
2024-02-26 12:23:31 -0500

    TEST UserControllerIntegrationTests create user

---
+++ src/test/java/com/example/voyages_api/controllers/UserControllerIntegrationTests.java
@@ -0,0 +1,48 @@
+package com.example.voyages_api.controllers;
+
+import com.example.voyages_api.TestDataUtil;
+import com.example.voyages_api.domain.entities.UserEntity;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+@SpringBootTest
+@ExtendWith(SpringExtension.class)
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
+@AutoConfigureMockMvc
+public class UserControllerIntegrationTests {
+
+    private final MockMvc mvc;
+
+    private final ObjectMapper objectMapper;
+
+    @Autowired
+    public UserControllerIntegrationTests(MockMvc mvc) {
+        this.mvc = mvc;
+        objectMapper = new ObjectMapper();
+    }
+
+    @Test
+    public void testThatCreateUserSuccessfullyReturnsHttp201Created() throws Exception {
+        UserEntity user = TestDataUtil.createTestUserEntityA();
+        user.setId(null);
+        String userJson = objectMapper.writeValueAsString(user);
+
+        mvc.perform(
+                MockMvcRequestBuilders.post("/users")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(userJson)
+        ).andExpect(
+                MockMvcResultMatchers.status().isCreated()
+        );
+    }
+}
