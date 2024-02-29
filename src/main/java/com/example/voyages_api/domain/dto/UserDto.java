dbf1dafe2e15f14da7da73391c42db2540c51420
Paul DAO <dao.van.paul@gmail.com>
2024-02-29 14:10:26 -0500

    UPDATE User Domain Entity and DTO with new columns in DB

--- src/main/java/com/example/voyages_api/domain/dto/UserDto.java
+++ src/main/java/com/example/voyages_api/domain/dto/UserDto.java
@@ -5,11 +5,17 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.Date;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
 public class UserDto {
     private Long id;
-    private String name;
+    private String firstname;
+    private String lastname;
+    private String email;
+    private Date createdDate;
+    private Date updatedDate;
 }
--- src/main/java/com/example/voyages_api/domain/entities/UserEntity.java
+++ src/main/java/com/example/voyages_api/domain/entities/UserEntity.java
@@ -1,12 +1,19 @@
 package com.example.voyages_api.domain.entities;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.util.Date;
 
 @Data
+@EntityListeners(AuditingEntityListener.class)
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
@@ -17,5 +24,23 @@ public class UserEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "user_id_seq")
     private Long id;
-    private String name;
+
+    @JsonProperty("firstname")
+    private String firstName;
+
+    @JsonProperty("lastname")
+    private String lastName;
+
+    private String email;
+
+    @CreatedDate
+    @Column(name = "created_date", nullable = false, updatable = false)
+    @Temporal(TemporalType.TIMESTAMP)
+    @JsonProperty("createdDate")
+    private Date createdDate;
+
+    @LastModifiedDate
+    @JsonProperty("updatedDate")
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date updatedDate;
 }
